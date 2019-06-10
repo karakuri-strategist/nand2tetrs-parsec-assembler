2a2467f9b297de2a86383ffa5a0678e0cc9847ef
Alec Featherston <alecfeatherston@gmail.com>
2019-06-09 21:07:09 -0700

    changed to take path and write to same file name with different extension

--- app/Main.hs
+++ app/Main.hs
@@ -5,17 +5,26 @@ import Parser (myParse)
 import System.IO (writeFile)
 import System.Environment (getArgs)
 import System.Exit (exitSuccess)
+import Data.List (last, intercalate)
+import Data.List.Split (splitOn)
 
 main :: IO ()
 main = do
-    input <- getContents
-    case myParse "(stdin)" input of
-        Left err -> do
-            putStrLn "failed to parse"
-            print err
-        Right tree -> do
-            case compile tree of
-                Left err -> putStrLn "failed to compile" >> putStrLn err
-                Right result -> putStr result
+    args <- getArgs
+    case args of
+        [filePath] -> do
+            let splitPath = splitOn "/" filePath
+                pathBefore = init splitPath
+                fileName = head $ splitOn "." (last splitPath)
+                writePath = intercalate "/" (pathBefore ++ [fileName ++ ".hack"])
+            input <- readFile filePath
+            case myParse "(stdin)" input of
+                Left err -> do
+                    putStrLn "failed to parse"
+                    print err
+                Right tree -> do
+                    case compile tree of
+                        Left err -> putStrLn "failed to compile" >> putStrLn err
+                        Right result -> writeFile writePath result 
+        _ -> putStrLn "You must provide exactly one argument, the path of the file to compile."
 
--- main = putStrLn "please what is going on"
